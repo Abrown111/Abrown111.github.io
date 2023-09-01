35c66e45d6b480af4d2e2b5515fae5d5cc747eb3
Abrown111 <100891622+Abrown111@users.noreply.github.com>
2023-09-01 06:09:23 -0400

    Added files for website

---
+++ script.js
@@ -0,0 +1,15 @@
+function scrollOnClickProjects(){
+  scroll(0, 650);
+}
+
+function scrollOnClickAbout(){
+  scroll(0, 1585);
+}
+
+function scrollOnClickContact(){
+  scroll(0, 2200);
+}
+
+function scrollOnClickHome(){
+  scroll(0, 0);
+}
